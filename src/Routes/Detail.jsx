e57b790578e7e9b4c0b582c2d05ec7a0cff35d21
Manesp <mespina98@gmail.com>
2023-06-29 20:36:49 -0300

    details finished

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -2,25 +2,31 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
-
-//Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
-
 const Detail = () => {
- 
+
   const params = useParams()
-  const[dentist, setDentist] = useState({})
+  const [dentist, setDentist] = useState({})
   const odontoURL = "https://jsonplaceholder.typicode.com/users/" + params.id
 
   useEffect(() => {
     axios(odontoURL).then(res => setDentist(res.data))
   })
+
+  console.log(dentist)
   return (
     <>
-      <h1>Detail Dentist {dentist.name} </h1>
-      <div>
-        <span>{dentist}</span>
+      <h1>Detail Dentist</h1>
+      <div className='odontoContainer'>
+        <div>
+          <h3>{dentist.name}</h3>
+          <p>Phone: {dentist.phone}</p>
+          <p>Website: {dentist.website}</p>
+          <p>E-mail: {dentist.email}</p>
+        </div>
+        <div style={{ width: '250px' }}>
+          <img src="../images/doctor.jpg" alt="doc image" style={{ width: '100%' }}></img>
+        </div>
       </div>
-      {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
     </>
   )
 }
